e8402e88f93f5d63fc34b5807e6ca5a5e912c12b
anketmanoj <anket.m@live.com>
2020-09-25 18:29:37 +0400

    Adding banners and CSS changes

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 const Error = () => {
     return (
         <div>
-            <Hero/>
+            <Hero>
+                <Banner title="Error 404" subtitle="page not found">
+                    <Link to='/' className="btn-primary">
+                        Return Home
+                    </Link>
+                </Banner>
+            </Hero>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-            <Hero/>
+            <Hero>
+                <Banner title="Luxurious Rooms" subtitle="Deluxe rooms starting at $199">
+                    <Link to="/rooms" className="btn-primary">
+                        Our rooms
+                    </Link>
+                </Banner>
+            </Hero>
             
         </div>
     )
