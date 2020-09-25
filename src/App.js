24800efea45ddd30896e208269e4ea44cc5666ed
anketmanoj <anket.m@live.com>
2020-09-25 11:37:50 +0400

    adding components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Home/>
+      <Error/>
+      <Rooms/>
+      <SingleRoom/>
     </div>
   );
 }
---
+++ src/pages/Rooms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rooms = () => {
+    return (
+        <div>
+            <h1>HEllo from rooms page</h1>
+        </div>
+    )
+}
+
+export default Rooms;
