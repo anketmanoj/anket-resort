33b302da3ae6ad87b1e5706037c320d7cf94e405
anketmanoj <anket.m@live.com>
2020-09-25 18:46:10 +0400

    adding services component

---
+++ src/components/Services.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import Title from './Title';
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from 'react-icons/fa';
+
+export default class Services extends Component {
+    state={
+        services:[
+            {
+                icon:<FaCocktail/>,
+                title:"Free Cocktails",
+                info: "Lorem ipsum nisi dolor aliquip sit tempor in laboris anim id ullamco est amet."
+            },
+            {
+                icon:<FaHiking/>,
+                title:"Incredile trails",
+                info: "Lorem ipsum nisi dolor aliquip sit tempor in laboris anim id ullamco est amet."
+            },
+            {
+                icon:<FaShuttleVan/>,
+                title:"24/7 Shuttle Van",
+                info: "Lorem ipsum nisi dolor aliquip sit tempor in laboris anim id ullamco est amet."
+            },
+            {
+                icon:<FaBeer/>,
+                title:"Strongest Beer",
+                info: "Lorem ipsum nisi dolor aliquip sit tempor in laboris anim id ullamco est amet."
+            },
+        ]
+    }
+    render() {
+        return (
+            <section className="services">
+                <Title title="Services"/>
+                <div className="services-center">
+                    {this.state.services.map((item, index) => {
+                        return <article key={index} className="service">
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </article>
+                    })}
+                </div>
+            </section>
+        )
+    }
+}
