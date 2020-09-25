1fb12cbfcb7f8e91869991dd01142ade543dbfaa
anketmanoj <anket.m@live.com>
2020-09-25 12:01:57 +0400

    working with recat router dom

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import Home from './pages/Home';
 import Error from './pages/Error';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
+import Navbar from './components/Navbar';
+
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Home/>
-      <Error/>
-      <Rooms/>
-      <SingleRoom/>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms" component={Rooms}/>
+        <Route exact path="/rooms/:slug" component={SingleRoom}/>
+        <Route component={Error}/>
+      </Switch>
     </div>
   );
 }
