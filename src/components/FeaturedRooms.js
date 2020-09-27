28a46d4a56bb466d752bdae88785ecfc9d981218
anketmanoj <anket.m@live.com>
2020-09-27 23:42:20 +0400

    Rooms list added

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -14,7 +14,7 @@ export default class FeaturedRooms extends Component {
         return (
             <section className='featured-rooms'>
                 <Title title="Featured rooms"/>
-                <div className='featured-rooms-center'>
+                <div data-tilt className ='featured-rooms-center'>
                     {loading ? <Loading/> : rooms}
                 </div>
             </section>
