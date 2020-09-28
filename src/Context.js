44dfb6315cb444ff0b11dd22438ca9050e95ef8c
anketmanoj <anket.m@live.com>
2020-09-28 11:33:56 +0400

    added rooms filter component

--- src/Context.js
+++ src/Context.js
@@ -57,14 +57,59 @@ class RoomProvider extends Component {
     };
 
     handleChange = (event) => {
-        const type = event.target.type;
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = event.target.name;
-        const value = event.target.value;
-        console.log(type, name, value);
-    }
+        this.setState({
+            [name]: value
+        }, 
+        this.filterRooms)        
+        // console.log(`this is type: ${type}, this is name: ${name}, this is value: ${value}`);
+    };
 
     filterRooms = () => {
-        console.log("Hello");
+        // console.log("Hello");
+        let {
+            rooms, type, capacity, price, minSize, maxSize, breakfast, pets
+        } = this.state;
+
+        //all the rooms
+        let tempRooms = [...rooms];
+        //transform values
+
+        capacity = parseInt(capacity);
+        price = parseInt(price);
+
+        //filter by type
+        if(type !== 'all'){
+            tempRooms = tempRooms.filter(room => room.type === type)
+        }
+
+        //filter by capacity
+        if(capacity !== 1){
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+        }
+
+        //filter by price
+        tempRooms = tempRooms.filter(room => room.price < price);
+
+        // filter by size
+        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+
+        //filter by breakfast
+        if(breakfast){
+            tempRooms = tempRooms.filter(room => room.breakfast === true)
+        }
+
+        //filter by pets
+        if(pets){
+            tempRooms = tempRooms.filter(room => room.pets === true)
+        }
+
+        //change state
+        this.setState({
+            sortedRooms: tempRooms
+        });
     }
 
     render() {
--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -24,6 +24,13 @@ export default function RoomsFilter({rooms}) {
       {item}
     </option>
   ));
+  
+  let people = getUnique(rooms, 'capacity');
+  people = people.map((item, index) => {
+      return(
+          <option key={index} value={item}>{item}</option>
+      )
+  })
 
     return (
         <section className="filter-container">
@@ -37,6 +44,55 @@ export default function RoomsFilter({rooms}) {
                     </select>
                 </div>
                 {/*end of select type */}
+                {/*guest */}
+                <div className="form-group">
+                    <label htmlFor="capacity">Guest</label>
+                    <select name="capacity" id="capacity" value={capacity} className="form-control" onChange={handleChange}>
+                        {people}
+                    </select>
+                </div>
+                {/*end of guest */}
+                {/* room price */}
+                <div className="form-group">
+                    <label htmlFor='price'>
+                        Room Price ${price}
+                    </label>
+                    <input type="range" name="price" min={minPrice} max={maxPrice} id="price" value={price} onChange={handleChange} className="form-control"/>
+                </div>
+                {/* end of room price */}
+                {/* size */}
+        <div className="form-group">
+          <label htmlFor="price">room size (SQFT)</label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              value={minSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+            <input
+              type="number"
+              name="maxSize"
+              value={maxSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+          </div>
+        </div>
+        {/* end of select type */}
+                {/* extras */}
+                <div className="form-group">
+                    <div className="single-extra">
+                        <input type="checkbox" name="breakfast" id="breakfast" checked={breakfast} onChange={handleChange}/>
+                        <label htmlFor="breakfast">breakfast</label>
+                    </div>
+                    <div className="single-extra">
+                        <input type="checkbox" name="pets" id="pets" checked={pets} onChange={handleChange}/>
+                        <label htmlFor="pets">pets</label>
+                    </div>
+                </div>
+                {/* end of extras */}
             </form>
         </section>
     )
